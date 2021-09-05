cc97c994529df5cdb1b8559f7af01afb26dd88c4
1000teslas <47207223+1000teslas@users.noreply.github.com>
2021-09-05 20:29:29 +1000

    api security in action chapter 2

--- natter-api/src/main/java/com/manning/apisecurityinaction/Main.java
+++ natter-api/src/main/java/com/manning/apisecurityinaction/Main.java
@@ -4,12 +4,18 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Objects;
 
 import com.manning.apisecurityinaction.controller.SpaceController;
 
 import org.dalesbred.Database;
+import org.dalesbred.result.EmptyResultException;
 import org.h2.jdbcx.JdbcConnectionPool;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import spark.Request;
+import spark.Response;
 
 import static spark.Spark.*;
 
@@ -24,16 +30,38 @@ public class Main {
         var spaceController = new SpaceController(database);
         post("/spaces", spaceController::createSpace);
 
-        after((request, response) -> {
-            response.type("application/json");
+        before((request, response) -> {
+            if (request.requestMethod().equals("POST") && !"application/json".equals(request.contentType())) {
+                halt(415, new JSONObject().put("error", "Only application/json supported").toString());
+            }
+        });
+
+        afterAfter((request, response) -> {
+            response.type("application/json;charset=utf-8");
+            response.header("X-Content-Type-Options", "nosniff");
+            response.header("X-Frame-Options", "DENY");
+            response.header("X-XSS-Protection", "0");
+            response.header("Cache-Control", "no-store");
+            response.header("Content-Security-Policy", "default-src 'none'; frame-ancestors 'none'; sandbox");
+            response.header("Server", "");
         });
 
         internalServerError(new JSONObject().put("error", "internal server error").toString());
         notFound(new JSONObject().put("error", "not found").toString());
+
+        exception(IllegalArgumentException.class, Main::badRequest);
+        exception(JSONException.class, Main::badRequest);
+        exception(EmptyResultException.class, (e, request, response) -> response.status(404));
     }
 
     private static void createTables(Database database) throws URISyntaxException, IOException {
+        @SuppressWarnings("nullable") // resource "/schema.sql" exists
         var path = Paths.get(Main.class.getResource("/schema.sql").toURI());
         database.update(Files.readString(path));
+    }
+
+    private static void badRequest(Exception ex, Request request, Response response) {
+        response.status(400);
+        response.body(new JSONObject().put("error", Objects.requireNonNullElse(ex.getMessage(), "")).toString());
     }
 }
